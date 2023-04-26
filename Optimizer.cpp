4652960352525980f9f17c7696d005ab79fc8a6c
Leonid <drfrily@gmail.com>
2023-04-27 00:21:47 +0300

    update Optimizer.cpp

--- Optimizer.cpp
+++ Optimizer.cpp
@@ -10,7 +10,7 @@ int main()
 	setlocale(LC_ALL, "Russian");
 
 	int sizeOfSimplex = 2;
-	double x0[2] = { 15.0, 15.0 }; // 1.0, 1.0
+	double x0[2] = { -17.7, -12.0 };
 	double epsilon = 0.1;
 	double sizeOfStep = 10;
 
